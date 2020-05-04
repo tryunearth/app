86e17a0a2afc86ba6379cfec4b146adaa6417873
Cedricium <amaya.cedric@gmail.com>
2020-05-04 13:15:53 -0700

    Fix: convert HTML properties to camel case

--- src/theme.js
+++ src/theme.js
@@ -7,9 +7,9 @@ const customIcons = {
       <g
         fill='none'
         stroke='currentColor'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2.5'
       >
         <path d='M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z' />
         <path d='M9 22V12h6v10' />
@@ -21,9 +21,9 @@ const customIcons = {
       <g
         fill='none'
         stroke='currentColor'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2.5'
       >
         <path d='M7.86 2h8.28L22 7.86v8.28L16.14 22H7.86L2 16.14V7.86L7.86 2zM12 8v4M12 16h.01' />
       </g>
@@ -34,9 +34,9 @@ const customIcons = {
       <g
         fill='none'
         stroke='currentColor'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2.5'
       >
         <path d='M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z' />
       </g>
@@ -47,9 +47,9 @@ const customIcons = {
       <g
         fill='none'
         stroke='currentColor'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2.5'
       >
         <path d='M6 9l6 6 6-6' />
       </g>
@@ -60,9 +60,9 @@ const customIcons = {
       <g
         fill='none'
         stroke='currentColor'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2.5'
       >
         <path d='M9 18l6-6-6-6' />
       </g>
