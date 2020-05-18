c8debab1bcfa9d45d2adf13bbcc1b52029e203cb
Cedricium <amaya.cedric@gmail.com>
2020-05-17 21:48:37 -0700

    Fix terribly-slow renders by using `react-virtualized`

--- src/components/ThingsList.js
+++ src/components/ThingsList.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import { Box, Flex, Image, Text, Link, Badge } from '@chakra-ui/core'
+import {
+  List,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+} from 'react-virtualized'
 
 import { useThings } from '../contexts/ThingsContext'
 import { EmptyState, LoadingState } from '../components'
@@ -71,10 +77,53 @@ const Thing = React.memo(({ thing }) => (
 
 const ThingsList = () => {
   const { things, isLoading } = useThings()
+  const PAGE_HEADING_HEIGHT = 45 + 16
+  const cache = new CellMeasurerCache({
+    fixedWidth: true,
+    minWidth: 400,
+    defaultHeight: 200,
+  })
+
+  const rowRenderer = ({ index, isScrolling, key, parent, style }) => {
+    const thing = things[index]
+    const content = <Thing thing={thing} />
+
+    return (
+      <CellMeasurer
+        key={key}
+        cache={cache}
+        parent={parent}
+        columnIndex={0}
+        rowIndex={index}
+      >
+        {({ registerChild }) => (
+          <div ref={registerChild} style={style}>
+            {content}
+          </div>
+        )}
+      </CellMeasurer>
+    )
+  }
+
   if (isLoading) return <LoadingState />
   if (things.length === 0) return <EmptyState />
   // TODO - use show/hide NSFW preference to filter nsfw content (show_nsfw_content: Boolean)
-  return things.map((thing) => <Thing key={thing.id} thing={thing} />)
+  return (
+    <AutoSizer>
+      {({ height, width }) => (
+        <List
+          style={{ outline: 'none' }}
+          width={width}
+          height={height - PAGE_HEADING_HEIGHT}
+          deferredMeasurementCache={cache}
+          rowHeight={cache.rowHeight}
+          rowRenderer={rowRenderer}
+          rowCount={things.length}
+          overscanRowCount={10}
+        />
+      )}
+    </AutoSizer>
+  )
 }
 
 export default ThingsList
