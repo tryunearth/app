260f8491566d43fa6c22b1495961f99bfcf3fda2
Cedricium <amaya.cedric@gmail.com>
2020-06-28 20:52:08 -0700

    Refactor searchbar into its own component; always visible in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useNavigate } from '@reach/router'
 import {
   Box,
@@ -13,35 +13,15 @@ import {
   MenuDivider,
 } from '@chakra-ui/core'
 
-import {
-  Tooltip,
-  Button,
-  ButtonGroup,
-  IconButton,
-  Input,
-  useDisclosure,
-} from '@chakra-ui/core'
-
 import unearthLogo from '../assets/unearth-horizontal_lockup.svg'
 
 import { useAuth } from '../contexts/AuthContext'
-import { SyncButton } from '../components'
+import { SyncButton, SearchBar } from '../components'
 
 const Header = () => {
+  const show = false
   const navigate = useNavigate()
   const { user, logout } = useAuth()
-  const show = false
-
-  const { isOpen, onToggle } = useDisclosure()
-  const [searchQuery, setSearchQuery] = useState('')
-  const handleSearch = (e) => {
-    e.preventDefault()
-    if (searchQuery) {
-      navigate(`/search/${searchQuery}`)
-    } else {
-      onToggle()
-    }
-  }
 
   return (
     <Flex
@@ -57,7 +37,7 @@ const Header = () => {
       borderBottomStyle='solid'
       borderBottomColor='gray.200'
     >
-      <Flex flex={1} align='center' mr={5}>
+      <Flex maxW='250px' flex='1 0 auto' align='center' mr={8}>
         <Image src={unearthLogo} alt='Unearth logo' maxH='47px' />
       </Flex>
 
@@ -86,78 +66,36 @@ const Header = () => {
         </Box> */}
 
       <Box
-        flex={6}
+        flex='4 1 0'
         mt={{ base: 4, md: 0 }}
-        display={{ sm: show ? 'block' : 'none', md: 'flex' }}
+        display={{
+          sm: show ? 'block' : 'none',
+          md: 'flex',
+        }}
       >
-        <Box
-          as='form'
-          onSubmit={handleSearch}
-          w='full'
-          display={isOpen ? 'flex' : 'none'}
-          justifyContent='space-between'
-          alignItems='center'
-        >
-          <Input
-            ml={20}
-            w='3xl'
-            size='lg'
-            type='text'
-            fontSize='md'
-            variant='filled'
-            placeholder='Search'
-            _hover={{ bg: 'gray.100' }}
-            _focus={{ bg: 'gray.100' }}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <ButtonGroup>
-            <Button size='sm' onClick={onToggle}>
-              Cancel
-            </Button>
-            <Button size='sm' variantColor='blue' type='submit'>
-              Search
-            </Button>
-          </ButtonGroup>
-        </Box>
-        <Box
-          w='full'
-          display={isOpen ? 'none' : 'flex'}
-          justifyContent='flex-end'
-          alignItems='center'
-        >
-          <Tooltip
-            hasArrow
-            label='Search'
-            aria-label='Search'
-            placement='bottom'
-          >
-            <IconButton
-              mr={3}
-              aria-label='Search saves'
-              icon='search'
-              variant='ghost'
-              onClick={onToggle}
-            />
-          </Tooltip>
-          <SyncButton />
-          <Menu>
-            <MenuButton as='button'>
-              <Avatar name={user.username} src={user.avatar_img} size='sm' />
-            </MenuButton>
-            <MenuList placement='bottom-end'>
-              <MenuGroup title={`Hello, ${user.username}`}>
-                <MenuItem onClick={() => navigate('/account')}>
-                  Account
-                </MenuItem>
-                <MenuItem isDisabled title='Coming soon'>
-                  Preferences
-                </MenuItem>
-              </MenuGroup>
-              <MenuDivider />
-              <MenuItem onClick={logout}>Logout</MenuItem>
-            </MenuList>
-          </Menu>
-        </Box>
+        <Flex w='full' justify='space-between' align='center'>
+          <SearchBar />
+          <Flex flex={1}>
+            <SyncButton />
+            <Menu>
+              <MenuButton as='button'>
+                <Avatar name={user.username} src={user.avatar_img} size='sm' />
+              </MenuButton>
+              <MenuList placement='bottom-end'>
+                <MenuGroup title={`Hello, ${user.username}`}>
+                  <MenuItem onClick={() => navigate('/account')}>
+                    Account
+                  </MenuItem>
+                  <MenuItem isDisabled title='Coming soon'>
+                    Preferences
+                  </MenuItem>
+                </MenuGroup>
+                <MenuDivider />
+                <MenuItem onClick={logout}>Logout</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
       </Box>
     </Flex>
   )
